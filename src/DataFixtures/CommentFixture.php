68f61cb4a5219af249e6f9a16156f39850720e9c
petrero <petrero@gmail.com>
2020-01-05 08:50:12 +0200

    7.4.Awesome Random Fixtures{DependentFixtureInterface}
    DependentFixtureInterface
    To squash previous commits problem as soon as you have a fixture class that is dependent on another fixture class, you need to implement an interface called DependentFixtureInterface.
    This will require you to have one method. Move to the bottom, then, go to the "Code" -> "Generate" menu, or Command + N on a Mac, select "Implement Methods" and choose getDependencies(). I'll add the public before the function.
    Just return an array with ArticleFixtures::class.
    That's it! Load them again:
    
    php bin/console doctrine:fixtures:load
    Bye bye error! It loaded ArticleFixtures first and then the comments below that. The fixtures library looks at all of the dependencies and figures out an order that makes sense.
    With that fixed, let's rename the class back from this ridiculous name to CommentFixture.
    To celebrate, move over, refresh and... awesome! 8, random comments. We rock!
    Next, let's learn about some tricks to control how Doctrine fetches the comments for an article, like, their order.

--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -4,9 +4,10 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class A0CommentFixture extends BaseFixture {
+class CommentFixture extends BaseFixture implements DependentFixtureInterface {
   protected function loadData(ObjectManager $manager){
     $this->createMany(Comment::class, 100,function (Comment $comment){
     	$comment->setContent(
@@ -19,4 +20,11 @@ class A0CommentFixture extends BaseFixture {
 
     $manager->flush();
   }
+
+	public function getDependencies() {
+		return [
+			ArticleFixtures::class
+		];
+	}
+
 }
