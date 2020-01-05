d425ffcf2eca90e698e2ad5471df5a5817a7da9c
petrero <petrero@gmail.com>
2020-01-05 12:03:25 +0200

    11.1.Twig Block Tricks{php bin/console make:controller}
    This tutorial is all about Doctrine relations... plus a few other Easter eggs along the way. And one big topic we need to talk about is how to create queries that join across these relationships. To do that properly, we're going to build a comment admin section. Well, for now, we're just going to start building it.
    Since this will be a new section on the site, let's create a new controller class! And because I'm feeling especially lazy, find your terminal and run:
    
    php bin/console make:controller
    Call it CommentAdminController. This creates a new class and one bonus template file. Go check it out!
    Ok, nice start! Hmm, but let's change that URL to /admin/comment.
    Let's see what we have so far. Open a new browser tab and go to http://localhost:8000/admin/comment. Awesome! The template even tells us where the source code lives!

---
+++ src/Controller/CommentAdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentAdminController extends AbstractController {
+  /**
+   * @Route("/admin/comment", name="comment_admin")
+   */
+  public function index() {
+    return $this->render('comment_admin/index.html.twig', [
+      'controller_name' => 'CommentAdminController',
+    ]);
+  }
+}
